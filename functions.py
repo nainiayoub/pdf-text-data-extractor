75ecf6f8b59eac21ffa49adaa4a0b5abb377398a
Ayoub NAINIA <nainiaayoub6@gmail.com>
2022-12-15 22:02:38 +0100

    undisplay doc when ocr enabled

--- functions.py
+++ functions.py
@@ -96,7 +96,7 @@ def save_pages(pages):
 def displayPDF(file):
   # Opening file from file path
   # with open(file, "rb") as f:
-  base64_pdf = base64.b64encode(file).decode('utf-8')
+  base64_pdf = base64.b64encode(file.read()).decode('utf-8')
 
   # Embedding PDF in HTML
   pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
