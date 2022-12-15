672b17db07e6f5bc9676f1f241f22bef8054eaf7
Ayoub NAINIA <nainiaayoub6@gmail.com>
2022-12-15 21:19:22 +0100

    app update

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pdf2image
-from functions import convert_pdf_to_txt_pages, convert_pdf_to_txt_file, save_pages, displayPDF, images_to_txt
+import pytesseract
+from pytesseract import Output, TesseractError
+from functions import convert_pdf_to_txt_pages, convert_pdf_to_txt_file, save_pages, displayPDF
 
 st.set_page_config(page_title="PDF to Text")
 
@@ -51,6 +53,21 @@ with st.sidebar:
         """,
         unsafe_allow_html=True,
     )
+if ocr_box:
+        option = st.selectbox('Select the document language', list(languages.keys()))
+@st.cache
+def images_to_txt(path):
+    images = pdf2image.convert_from_bytes(path)
+    all_text = []
+    for i in images:
+        pil_im = i
+        text = pytesseract.image_to_string(pil_im, lang=languages[option])
+        # ocr_dict = pytesseract.image_to_data(pil_im, lang='eng', output_type=Output.DICT)
+        # ocr_dict now holds all the OCR info including text and location on the image
+        # text = " ".join(ocr_dict['text'])
+        # text = re.sub('[ ]{2,}', '\n', text)
+        all_text.append(text)
+    return all_text, len(all_text)
 
 pdf_file = st.file_uploader("Load your PDF", type="pdf")
 hide="""
@@ -69,12 +86,11 @@ if pdf_file:
     # display document
     with st.expander("Display document"):
         displayPDF(pdf_file)
-    if ocr_box:
-        option = st.selectbox('Select the document language', list(languages.keys()))
+    
     # pdf to text
     if textOutput == 'One text file (.txt)':
         if ocr_box:
-            texts, nbPages = images_to_txt(pdf_file.read(), languages[option])
+            texts, nbPages = images_to_txt(pdf_file.read())
             totalPages = "Pages: "+str(nbPages)+" in total"
             text_data_f = "\n\n".join(texts)
         else:
@@ -85,7 +101,7 @@ if pdf_file:
         st.download_button("Download txt file", text_data_f)
     else:
         if ocr_box:
-            text_data, nbPages = images_to_txt(pdf_file.read(), languages[option])
+            text_data, nbPages = images_to_txt(pdf_file.read())
             totalPages = "Pages: "+str(nbPages)+" in total"
         else:
             text_data, nbPages = convert_pdf_to_txt_pages(pdf_file)
