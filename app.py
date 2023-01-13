b545229b85157ae202e4f0f1cd6ac636a0771491
Ayoub NAINIA <nainiaayoub6@gmail.com>
2023-01-13 11:02:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -45,14 +45,7 @@ with st.sidebar:
     st.markdown("""
     Made by [@nainia_ayoub](https://twitter.com/nainia_ayoub) 
     """)
-    st.markdown(
-        """
-        <a href="https://www.buymeacoffee.com/nainiayoub" target="_blank">
-        <img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="41" width="174">
-        </a>
-        """,
-        unsafe_allow_html=True,
-    )
+    
 
 pdf_file = st.file_uploader("Load your PDF", type="pdf")
 hide="""
